580f23412d3b63ffc6f9e3432fdb24be994992db
Bozska_XD <85171769+BozskaTheLOL@users.noreply.github.com>
2024-10-04 11:00:43 +0200

    SQL File Upload

---
+++ harmony.sql
@@ -0,0 +1,116 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2024. Okt 04. 10:37
+-- Kiszolgáló verziója: 10.4.6-MariaDB
+-- PHP verzió: 7.3.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `harmony`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `login`
+--
+
+CREATE TABLE `login` (
+  `lid` int(11) NOT NULL,
+  `ulid` int(11) NOT NULL,
+  `ldate` datetime NOT NULL,
+  `lip` varchar(40) COLLATE utf8_hungarian_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `naplo`
+--
+
+CREATE TABLE `naplo` (
+  `nid` int(11) NOT NULL,
+  `nuid` int(11) NOT NULL,
+  `ndate` datetime NOT NULL,
+  `nip` varchar(40) COLLATE utf8_hungarian_ci NOT NULL,
+  `nurl` varchar(250) COLLATE utf8_hungarian_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `user`
+--
+
+CREATE TABLE `user` (
+  `uid` int(11) NOT NULL,
+  `unick` varchar(18) COLLATE utf8_hungarian_ci NOT NULL,
+  `upw` varchar(40) COLLATE utf8_hungarian_ci NOT NULL,
+  `umail` varchar(100) COLLATE utf8_hungarian_ci NOT NULL,
+  `udate` datetime NOT NULL,
+  `uip` varchar(40) COLLATE utf8_hungarian_ci NOT NULL,
+  `ustatus` varchar(3) COLLATE utf8_hungarian_ci NOT NULL,
+  `ukomment` varchar(100) COLLATE utf8_hungarian_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`lid`);
+
+--
+-- A tábla indexei `naplo`
+--
+ALTER TABLE `naplo`
+  ADD PRIMARY KEY (`nid`);
+
+--
+-- A tábla indexei `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `login`
+--
+ALTER TABLE `login`
+  MODIFY `lid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `naplo`
+--
+ALTER TABLE `naplo`
+  MODIFY `nid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `user`
+--
+ALTER TABLE `user`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
